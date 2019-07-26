4a0868463278d493e926c863e6c7d3103c5b5c84
Will Hodgson <will.hodgson@current-rms.com>
2019-07-26 14:59:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   when 1
     array[0]
   when 2
-    "#{array[0] and array[1]}"
+    "#{array[0]} and #{array[1]}"
   else
     array[array.length - 1] = "and #{array.last}"
     array.join(', ')
