0043171185672d4ddd5230000f9e2bfb23f51b74
Will Hodgson <will.hodgson@current-rms.com>
2019-07-26 14:58:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,11 @@
 def oxford_comma(array)
-  array[array.length - 1] = "and #{array.last}"
-  array.join(', ')
+  case array.length
+  when 1
+    array[0]
+  when 2
+
+  else
+    array[array.length - 1] = "and #{array.last}"
+    array.join(', ')
+  end
 end
